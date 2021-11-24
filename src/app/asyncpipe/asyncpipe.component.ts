aed00d8f70869d1013a73bc6d57396bfbbc5e75c
Newell Jones <njones@zekiah.com>
2021-11-24 14:25:59 -0500

    Two solid methods of displaying time (prototype is not modern)

--- src/app/asyncpipe/asyncpipe.component.ts
+++ src/app/asyncpipe/asyncpipe.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-/* https://angular.io/guide/observables-in-angular */
+import { Subject, Subscription, timer } from 'rxjs';
+import { map, share } from "rxjs/operators";
+/* 
+https://angular.io/guide/observables-in-angular 
+Clocks
+https://stackblitz.com/edit/angular-ihpcnz
+*/
 
-import { ZuluDateTimeFormatPipe } from '../shared/zulu-date-time-pipe/zulu-date-time-pipe';
+// import { ZuluDateTimeFormatPipe } from '../shared/zulu-date-time-pipe/zulu-date-time-pipe';
 
 @Component({
   selector: 'app-asyncpipe',
@@ -10,20 +15,38 @@ import { ZuluDateTimeFormatPipe } from '../shared/zulu-date-time-pipe/zulu-date-
   styleUrls: ['./asyncpipe.component.css']
 })
 export class AsyncpipeComponent implements OnInit {
-  
-  time = new Observable<string>(observer => {
-    setInterval(() => observer.next(new Date().toString()), 1000);
-  });
-
-  // transform(dateIn, 'timestamp')
-  zulu = new Observable<string>(observer => {
-    setInterval(() => observer.next(ZuluDateTimeFormatPipe.prototype.transform( new Date() )), 1000);
-  });
+  /* https://stackoverflow.com/questions/948532/how-do-you-convert-a-javascript-date-to-utc/11957822#11957822 */
+  time = new Date().toISOString();
+  rxTime = new Date().toISOString();
+  intervalId: any;
+  unsubscribe$ = new Subject<void>();
+  subscription: Subscription[] = [];
 
   constructor( ) { }
 
   ngOnInit(): void { 
-    
+
+    // Using Basic Interval
+    this.intervalId = setInterval(() => {
+      this.time = new Date().toISOString();
+    }, 1000);
+
+    // Using RxJS Timer
+    this.subscription.push(timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.rxTime = time.toISOString();
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
